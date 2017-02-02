070695dd29b1a821674851985687c4b22260e780
Jigmee Sherpa <Jigmee Sherpa@Yui>
2017-02-02 12:56:19 +0545

    Update

--- view.php
+++ view.php
@@ -1,54 +1,68 @@
-
 <?php
-$con = mysqli_connect('localhost','root','','practice');
+$con = mysqli_connect("localhost", "root", "");
+mysqli_select_db($con,"intern");
 
-	if(isset($_POST['insert_post'])){
-
-	$from_date = $_POST['from_date'];
-	$to_date = $_POST['to_date'];
-	$value = $_POST['value'];
-
-	$form = "INSERT INTO date1 (from_date,to_date,value) values ('$from_date','$to_date','$value')";
-
-	$insert_pro = mysqli_query($con, $form);
-	}
 
 ?>
+
 <!DOCTYPE html>
 
 <html>
 <head>
-	<title>View Value</title>
+	<title>Form</title>
+	
 </head>
 <body>
 
-<table align="center" width="700" border="2">
-	<tr>
-		<td>From_date</td>
-		<td>To_date</td>
-		<td>Value</td>
-	</tr>
+
+<form name="form1" action="index.php" method="post">
+
+<h4>From-date:</h4>
+<select>
+	<?php
+	$result1 = mysqli_query($con,"SELECT * FROM datelist");
+	while($row = mysqli_fetch_array($result1))
+	{
+	?>
+	
+	<option><?php echo $row["date"]; ?></option>
 	
 	<?php
-	$sn = 1;
-	$get_pro = "select * from date1";
-	$run_pro = mysqli_query($con,$get_pro);
+	}
+	?>
 
-	while($row_pro = mysqli_fetch_array($run_pro))
+</select>
+ 
+<h4>To-date:</h4>
+<select>
+	<?php
+	$result2 = mysqli_query($con,"SELECT * FROM dateList");
+	while($row = mysqli_fetch_array($result2))
 	{
-		
+	?>
+	
+	<option><?php echo $row["date"]; ?></option>
+	
+	<?php
+	}
 	?>
 
+</select></br></br>
+
+<input type="submit" name="insert_post" value="Submit" />
+
+<h4>List of Values</h4>
+
+
+<table align="" width="700" border="2">
 	<tr>
-		<td><?php echo $row_pro['from_date']; ?></td>
-		<td><?php echo $row_pro['to_date']; ?></td>
-		<td><?php echo $row_pro['value']; ?></td>
+		<th>Date</th>
+		<th>Value</th>
+		
 	</tr>
-	<?php 
-	$sn++;}
 
-	?>
-</table>
+
+</form>
 
 </body>
 </html>
